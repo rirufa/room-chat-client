19740dc39d544b229ae806e1b53f89fbaa6e0708
yosuke <yosuke@cowcowhomes.webcrow.jp>
2019-12-10 14:14:48 +0900

    apollo-clientを使うようにした

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,17 +1,34 @@
 import AppConfig from './AppConfig';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import gql from 'graphql-tag';
 
-const client = new ApolloClient({
+const cache = new InMemoryCache();
+const httpLink = new HttpLink({
   uri: AppConfig.GRAPHQL_SERVER,
-  request: operation => {
-    let token = localStorage.token;
-    operation.setContext({
-      headers: {
-        "x-access-token": token
-      }
-    });
+});
+const authLink = setContext((_, { headers }) => {
+  let token = localStorage.token;
+  return {
+    headers: {
+      ...headers,
+      "x-access-token": token
+    }
   }
+});
+const client = new ApolloClient({
+  cache: cache,
+  link: authLink.concat(httpLink),
+  name: 'react-web-client',
+  version: '1.3',
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
 });
 
 export class ApiClient
